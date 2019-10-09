438ba47da4d40cb905ecaeeff10b64485437028c
Adriaan Slechten <adriaan.slechten@businessdecision.be>
2019-10-09 16:54:57 +0200

    refactored the increment methods to take into account boolean + tests

--- python/src/gilded_rose.py
+++ python/src/gilded_rose.py
@@ -12,7 +12,10 @@ class GildedRose(object):
         :param item: item which we want tod descrease
         :return: integer, containing the quality minus one
         """
-        return item.quality -1
+        if item.quality > 0:
+            return item.quality -1
+        else:
+            return item.quality
 
     def increase_quality(self,item):
         """
@@ -20,36 +23,31 @@ class GildedRose(object):
         :param item: item which we want to increase
         :return: integer, containing the quality plus one
         """
-        return item.quality +1
+        if item.quality < 50:
+            return item.quality +1
+        else:
+            return item.quality
 
     def update_quality(self):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = self.decrease_quality(item)
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
+                    item.quality = self.increase_quality(item)
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = self.increase_quality(item)
-                        if item.sell_in < 6:
-                            if item.quality < 50:
                                 item.quality = self.increase_quality(item)
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
                                 item.quality = self.decrease_quality(item)
                     else:
                         item.quality = item.quality - item.quality
                 else:
-                    if item.quality < 50:
                         item.quality = self.increase_quality(item)
 
 
