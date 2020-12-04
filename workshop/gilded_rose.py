651aeca93525eaadf294f3fde9b38d838bfd82e6
kevin.neville <kevin.neville@klarna.com>
2020-12-04 16:40:01 +0100

    Move item class to seperate module

--- workshop/gilded_rose.py
+++ workshop/gilded_rose.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from workshop.items.items import Item
 from workshop.libs.item_helpers import (
     decrease_quality,
     decrease_sell_in,
@@ -69,24 +70,3 @@ class GildedRose(object):
         for item in self.items:
             self.item = self.update_item_quality(item)
         return self.items
-
-
-class Item:
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-
-    def __eq__(self, other):
-        return (
-            self.name == other.name
-            and self.sell_in == other.sell_in
-            and self.quality == other.quality
-        )
-
-    def __hash__(self):
-        # necessary for instances to behave sanely in dicts and sets.
-        return hash((self.name, self.sell_in, self.quality))
-
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
