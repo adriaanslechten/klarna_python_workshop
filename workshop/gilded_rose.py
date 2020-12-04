e1f5df75967308dcdeb915a1a5be83e92618c938
Adriaan Slechten <adriaan.slechten@gmail.com>
2020-12-04 14:34:59 +0100

    Identify sell-in location and order where it needs to happen.

--- workshop/gilded_rose.py
+++ workshop/gilded_rose.py
@@ -31,15 +31,17 @@ class GildedRose(object):
             else:
                 if item.name == "Aged Brie":
                     item.quality = self.increase_quality(item)
+                    item.sell_in = item.sell_in - 1
                 elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                     item.quality = self.increase_quality(item)
                     if item.sell_in < 11:
                         item.quality = self.increase_quality(item)
                     if item.sell_in < 6:
                         item.quality = self.increase_quality(item)
+                    item.sell_in = item.sell_in - 1
                 else:
                     item.quality = self.decrease_quality(item)
-                item.sell_in = item.sell_in - 1
+                    item.sell_in = item.sell_in - 1
                 if item.sell_in < 0:
                     if item.name == "Aged Brie":
                         item.quality = self.increase_quality(item)
