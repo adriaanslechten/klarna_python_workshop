a65251e6929884832839604f1c9000a8a8770a6f
Adriaan Slechten <adriaan.slechten@gmail.com>
2020-12-04 13:36:13 +0100

    Fixed first unit test.

--- test/test_gilded_rose.py
+++ test/test_gilded_rose.py
@@ -5,4 +5,4 @@ def test_foo():
     items = [Item("foo", 0, 0)]
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
-    assert "FIXME" == items[0].name
+    assert "foo" == items[0].name
