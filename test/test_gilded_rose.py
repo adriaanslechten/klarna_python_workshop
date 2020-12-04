23d957ffa51e6acdd34a22261f2401357e0ec5f1
Adriaan Slechten <adriaan.slechten@gmail.com>
2020-12-04 13:41:05 +0100

    Factored out repeated code wrt. quality + tests.

--- test/test_gilded_rose.py
+++ test/test_gilded_rose.py
@@ -6,3 +6,19 @@ def test_foo():
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
     assert "foo" == items[0].name
+
+
+def test_increase_quality():
+    item = Item(name="+5 Dexterity Vest", sell_in=10, quality=20)
+    items = [item]
+    gilded_rose = GildedRose(items)
+    actual = gilded_rose.increase_quality(item)
+    assert actual == 21
+
+
+def test_decrease_quality():
+    item = Item(name="+5 Dexterity Vest", sell_in=10, quality=20)
+    items = [item]
+    gilded_rose = GildedRose(items)
+    actual = gilded_rose.decrease_quality(item)
+    assert actual == 19
